80f4b0f5ecdf5d66c31fef77c289cb09e1847182
Öznur Yeniler <oznuryeniler@MacBook-Pro-von-Oznur.fritz.box>
2026-01-07 23:36:52 +0100

    test: update comments

--- app/(flow)/_layout.tsx
+++ app/(flow)/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 
 export default function FlowLayout() {
   return (
+    //Stack wird verwendet, weil die Prozesse Schritt für Schritt geführt werden
     <Stack screenOptions={{ headerShown: true }}>
       <Stack.Screen name="scan" options={{ title: "Scan" }} />
       <Stack.Screen name="quiz" options={{ title: "Quiz" }} />
